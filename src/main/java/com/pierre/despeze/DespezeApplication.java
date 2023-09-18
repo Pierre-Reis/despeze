0c9a7c1596c37cb08f58f8eb86efcbb3ffeb8029
Pierre Reis <pierre.developer@hotmail.com>
2023-09-18 19:21:54 -0300

    Project created

---
+++ src/main/java/com/pierre/despeze/DespezeApplication.java
@@ -0,0 +1,13 @@
+package com.pierre.despeze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespezeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DespezeApplication.class, args);
+	}
+
+}
