a7fca9a6d194fb659e264e78a177bd7ca2399afe
Pierre Reis <pierre.developer@hotmail.com>
2024-10-15 20:18:10 -0300

    Initial commit

---
+++ despeze/src/main/java/br/com/pierre/despeze/DespezeApplication.java
@@ -0,0 +1,13 @@
+package br.com.pierre.despeze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespezeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DespezeApplication.class, args);
+	}
+
+}
---
+++ despeze/src/test/java/br/com/pierre/despeze/DespezeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pierre.despeze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DespezeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
